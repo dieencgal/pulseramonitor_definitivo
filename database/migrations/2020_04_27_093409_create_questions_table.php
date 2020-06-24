3a48422dd63286787378a7d3d7d4d7fa93bb520c
diego <dieencgal@alum.us.es>
2020-06-24 11:27:47 +0200

    comentado una migracion q daba error

--- database/migrations/2020_04_27_093409_create_questions_table.php
+++ database/migrations/2020_04_27_093409_create_questions_table.php
@@ -13,17 +13,17 @@ class CreateQuestionsTable extends Migration
      */
     public function up()
     {
-       /* Schema::create(config('survey.database.tables.questions'), function (Blueprint $table) {
+       Schema::create(config('survey.database.tables.questions'), function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('survey_id')->nullable();
             $table->unsignedInteger('section_id')->nullable();
             $table->string('content');
             $table->string('type')->default('text');
-            $table->json('options')->nullable();
-            $table->json('rules')->nullable();
+            /*$table->json('options')->nullable();
+            $table->json('rules')->nullable();*/
             $table->timestamps();
         });
-       */
+
     }
 
     /**
